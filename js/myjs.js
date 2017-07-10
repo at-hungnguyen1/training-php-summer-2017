0231cbab39f17978099af83feb9ac53b469b9ce7
hungnguyen <hungbkdn1295@gmail.com>
2017-07-10 14:51:28 +0700

    Update json

--- js/myjs.js
+++ js/myjs.js
@@ -1,61 +1,22 @@
-var itemValue = [
-	{
-		image: "../images/new-1.jpg",
-		title: "Đau cổ vai tay",
-		description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
-	},
-	{
-		image: "../images/new-2.jpg",
-		title: "Đau cổ vai tay",
-		description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
-	},
-	{
-		image: "../images/new-3.jpg",
-		title: "Đau cổ vai tay",
-		description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
-	},
-	{
-		image: "../images/new-4.jpg",
-		title: "Đau cổ vai tay",
-		description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
-	},
-	{
-		image: "../images/new-5.jpg",
-		title: "Đau cổ vai tay",
-		description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
-	},
-	{
-		image: "../images/new-6.jpg",
-		title: "Đau cổ vai tay",
-		description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
-	},
-	{
-		image: "../images/new-7.jpg",
-		title: "Đau cổ vai tay",
-		description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
-	},
-	{
-		image: "../images/new-8.jpg",
-		title: "Đau cổ vai tay",
-		description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
-	},
-	{
-		image: "../images/new-9.jpg",
-		title: "Đau cổ vai tay",
-		description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna"
-	}
-];
-var x = document.getElementsByClassName("news-list");
-var innerToHTML = '';
-itemValue.forEach(function(element)
-	{
-		innerToHTML += '<li class ="item">' +
-		'<div class ="inner">' +
-		'<img src = "' + element.image + '">' +
-		'<h3>' + element.title + '</h3>' +
-		'<p>' + element.description + '</p>' +
-		'<div class="arrow">' +
-		'<a href="#"><img src = "../images/rightarrow.png"></a>' +
-		'</div></div></li>';
-	});
-x[0].innerHTML = innerToHTML;
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+if (this.readyState == 4 && this.status == 200) {
+  itemValue = JSON.parse(this.responseText);
+  var x = document.getElementsByClassName("news-list");
+	var innerToHTML = '';
+	itemValue.forEach(function(element)
+		{
+			innerToHTML += '<li class ="item">' +
+			'<div class ="inner">' +
+			'<img src = "' + element.image + '">' +
+			'<h3>' + element.title + '</h3>' +
+			'<p>' + element.description + '</p>' +
+			'<div class="arrow">' +
+			'<a href="#"><img src = "../images/rightarrow.png"></a>' +
+			'</div></div></li>';
+		});
+	x[0].innerHTML = innerToHTML;
+}
+};
+xhttp.open("GET", "../news.json", true);
+xhttp.send();
